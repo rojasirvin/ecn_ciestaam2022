fa815a6b666c63db2d0733672cf24add3bcdc206
rojasirvin <rojasirvin@gmail.com>
2022-02-01 16:47:41 -0600

    add sesion mco

---
+++ scripts/recordatorio_mco.R
@@ -0,0 +1,73 @@
+#Recordatorio de MCO
+
+#Iniciamos una nueva sesión quitando todos los elementos previos
+rm(list = ls()) 
+#Para evitar la notación científica
+options(scipen=999) 
+
+library(tidyverse)
+library(broom)
+
+
+#Simulación de datos de educación e ingreso
+
+#Fijo una semilla
+set.seed(1234)
+
+#Genero el vector de educación y una perturbación normal
+educacion <-  rnorm(100,10,3)
+e <-  rnorm(100,0,8)
+
+#Genero el salario con parámetros conocidos (b0=100, b1=2)
+b0 <- 150
+b1 <- 2
+
+salario <- b0 + b1 * educacion + e 
+datos.salarios <- as.data.frame(cbind(salario, educacion))
+  
+(lm(salario ~ educacion))
+
+
+
+
+#Gráfico de la relación educación e ingreso
+
+datos.salarios %>% 
+  ggplot(aes(x=educacion, y=salario )) +
+  geom_point()+
+  labs(x="Educación", "Salario por hora")
+
+
+
+
+#Estimación por MCO
+
+reg1 <- lm(salario ~ educacion,
+           data=datos.salarios)
+
+summary(reg1)
+
+
+
+#Gráfico con línea de regresión
+datos.salarios %>% 
+  ggplot(aes(x=educacion, y=salario )) +
+  geom_point()+
+  labs(x="Educación", "Salario por hora")+
+  geom_smooth(method = 'lm', se = F)
+
+
+
+
+#Visualización de residuales
+
+datos.salarios <- augment(reg1)
+
+datos.salarios %>% 
+  ggplot(aes(x=educacion, y=salario )) +
+  geom_point()+
+  labs(x="Educación", "Salario por hora")+
+  geom_smooth(method = 'lm', se = F)+
+  geom_segment(aes(xend = educacion, yend = .fitted), color = "red", size = 0.3)
+
+
